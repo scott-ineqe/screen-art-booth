940dde3ef2705fb68f716604e480177266b83446
scott-ineqe <scott@ineqe.com>
2026-03-26 11:48:19 +0000

    Update DeviceFrame.tsx

--- src/components/DeviceFrame.tsx
+++ src/components/DeviceFrame.tsx
@@ -24,7 +24,7 @@ const DEVICE_CONFIGS: Record<DeviceType, DeviceConfig> = {
     screenArea: { 
       top: "1.8%", 
       left: "2%", 
-      width: "90%", 
+      width: "85%", 
       height: "94%", 
       borderRadius: "44px" 
     }
